50384c188f3aa7193a6ca74f21aae38bdc58ee9a
locnelor <1750354949@qq.com>
2024-05-28 09:32:15 +0800

    fix(decorator): bug

--- src/toolbars/decorators/LinkDecorator.tsx
+++ src/toolbars/decorators/LinkDecorator.tsx
@@ -1,39 +1,33 @@
+import { styled } from '@stitches/react';
+import React from 'react';
+import withDecorator from './withDecorator';
 
-import { styled } from "@stitches/react";
-import React from "react";
-import { withDecorator } from "react-open-rich-editor";
-
-const StyledLink = styled("a", {
-    color: "#3f51bf",
-    cursor: "pointer"
-})
-const LinkDecorator = withDecorator(({
-    contentState,
-    children,
-    entityKey
-}) => {
+const StyledLink = styled('a', {
+  color: '#3f51bf',
+  cursor: 'pointer',
+});
+const LinkDecorator = withDecorator(
+  ({ contentState, children, entityKey }) => {
     const { href, ...rest } = contentState.getEntity(entityKey).getData();
     return (
-        <StyledLink
-            href={href}
-            {...rest}
-            target="_blank"
-            title={href}
-        >
-            {children}
-        </StyledLink>
-    )
-}, (block, callback, contentState) => {
-    block.findEntityRanges(
-        (character) => {
-            const entityKey = character.getEntity();
-            return (
-                entityKey !== null &&
-                contentState.getEntity(entityKey).getType() === 'LINK'
-            );
-        }, (...arr) => {
-            callback(...arr)
-        }
+      <StyledLink href={href} {...rest} target="_blank" title={href}>
+        {children}
+      </StyledLink>
     );
-})
-export default LinkDecorator
+  },
+  (block, callback, contentState) => {
+    block.findEntityRanges(
+      (character) => {
+        const entityKey = character.getEntity();
+        return (
+          entityKey !== null &&
+          contentState.getEntity(entityKey).getType() === 'LINK'
+        );
+      },
+      (...arr) => {
+        callback(...arr);
+      },
+    );
+  },
+);
+export default LinkDecorator;
